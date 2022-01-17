ed82f3062bc7480bb8606dafa6369bd779eac984
ilter <ilter.kose@ozu.edu.tr>
2022-01-17 07:04:38 +0300

    Initial Commit

---
+++ GithubProfileWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubProfileWiki
+//
+//  Created by ilter on 17.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
