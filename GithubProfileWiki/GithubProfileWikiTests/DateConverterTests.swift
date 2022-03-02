35977bdc3b24f3505ac1f8b8cf84763590402536
ilter <ilter.kose@ozu.edu.tr>
2022-03-03 02:02:29 +0300

    Refactor converToDate timezone

--- GithubProfileWiki/GithubProfileWikiTests/DateConverterTests.swift
+++ GithubProfileWiki/GithubProfileWikiTests/DateConverterTests.swift
@@ -19,7 +19,7 @@ extension String {
     func convertToDate() -> Date? {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        dateFormatter.locale = Locale(identifier: "en_GB")
+        dateFormatter.locale = Locale(identifier: "en_US")
         dateFormatter.timeZone = .current
 
         return dateFormatter.date(from: self)
